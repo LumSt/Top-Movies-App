67b5ec269506f2d9481480cb2b5c54b491e13079
Lum Situ <lumsitu@Lums-MacBook-Pro.local>
2017-01-30 23:47:01 -0800

    Change to CollectionView

---
+++ Top Movies App/MovieCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCollectionViewCell.swift
+//  Top Movies App
+//
+//  Created by Lum Situ on 1/30/17.
+//  Copyright © 2017 Lum Situ. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var posterCollectionView: UIImageView!
+    
+}
